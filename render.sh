883a2bfbc743eb57916a3ccdfb8601e537ab297d
Liam Bowen <LiamBowen@gmail.com>
2014-06-18 11:14:40 -0400

    enscript doesn't return 0 on success?!

--- render.sh
+++ render.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-enscript --no-header --output=resume.ps --margins=1:1:1:1 --title='Liam Bowen - Resume' resume.txt && ps2pdf resume.ps
+enscript --no-header --output=resume.ps --margins=1:1:1:1 --title='Liam Bowen - Resume' resume.txt
+ps2pdf resume.ps
