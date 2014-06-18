836ae3e73b0af859b892f67876aa44ab6783ae8f
Liam Bowen <LiamBowen@gmail.com>
2014-06-18 11:01:34 -0400

    Render a PDF too

--- render.sh
+++ render.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-enscript --no-header --output=resume.ps --margins=1:1:1:1 --title='Liam Bowen - Resume' resume.txt
+enscript --no-header --output=resume.ps --margins=1:1:1:1 --title='Liam Bowen - Resume' resume.txt && ps2pdf resume.ps
